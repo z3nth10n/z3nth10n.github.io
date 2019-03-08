97c97a265b0af79341edbddf0b05e83ac544665c
Ikillnukes <ikillnukes1@gmail.com>
2019-03-08 04:32:05 +0100

    Trying to call redirects from xml

--- js/lang.redirect.js
+++ js/lang.redirect.js
@@ -1,12 +1,14 @@
-(function() {
+function redirectLang(relUrl) {
     var langArr = ["en", "es"]; // TODO: 0 Scalibity
     var langs = Enumerable.from(window.location.href.split("/")).where(p => langArr.includes(p));
-
+    
+    console.log(langs.count());
+    
     if(langs.count() == 0)
     {
-        var url = window.location.protocol + "//" + window.location.host + "/en/";
+        var url = window.location.protocol + "//" + window.location.host + "/en/" + relUrl;
         
         console.log(url);
         // window.location.href = url;
     }
-})();
+}
