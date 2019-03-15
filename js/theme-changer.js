6cf185f08c1a53a137d24a360be6ed6aa1e7b7c6
Ikillnukes <ikillnukes1@gmail.com>
2019-03-15 07:33:25 +0100

    Storing themes in cookies

--- js/theme-changer.js
+++ js/theme-changer.js
@@ -2,10 +2,16 @@ $(document).ready(function() {
    $("body").addClass("net default js-theme-wrapper");
     
     (function(exports) {
+      function setTheme(_themeChanger, theme) {
+        console.log("Setting theme: " + theme);
+        _themeChanger.settings.wrapper.removeClass().addClass('net ' + theme);
+      }   
+    
       var themeChanger = {
         settings: {
           wrapper: $('.js-theme-wrapper'),
-          buttons: $('.js-theme-button')
+          buttons: $('.js-theme-button'),
+          cookieName: "current-theme"    
         },
 
         init: function () {
@@ -13,7 +19,9 @@ $(document).ready(function() {
           this.settings.buttons.on('click', function () {
             var $node = $(this),
                 theme = $node.data('theme');
-            _self.settings.wrapper.removeClass().addClass('net ' + theme);
+            
+            setTheme(_self, theme);      
+            Cookies.set(_self.settings.cookieName, theme);
             //_self.settings.buttons.removeAttr('disabled');
             //$node.attr('disabled', true);
           });
@@ -21,5 +29,12 @@ $(document).ready(function() {
       };
 
       themeChanger.init();
+        
+      var currentTheme = Cookies.get(themeChanger.settings.cookieName);
+        
+      console.log("Cookie stored theme: " + currentTheme);
+        
+      if(currentTheme != undefined)
+        setTheme(themeChanger, currentTheme); 
     }(window));
 });
