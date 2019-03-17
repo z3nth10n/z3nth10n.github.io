d5108e39932bde2a320f113e66136e9a868c4c7f
Ikillnukes <ikillnukes1@gmail.com>
2019-03-18 00:07:04 +0100

    Translating Patreon (first part)

--- _plugins/jekyll-patreon/lib/jekyll-patreon/tags/patreonTag.rb
+++ _plugins/jekyll-patreon/lib/jekyll-patreon/tags/patreonTag.rb
@@ -7,7 +7,6 @@
 #     {% patreon z3nth10n %}
 #   Output: Patreon donation widget
 
-require "jekyll-patreon/version"
 require 'net/http'
 
 module Jekyll
@@ -22,7 +21,7 @@ module Jekyll
         def initialize(tag_name, markup, tokens)
           super
             
-          @inc = File.expand_path("../../_inc", __FILE__)
+          @inc = File.expand_path(File.join("..", "..", "_inc"), __FILE__)
             
           @config = Jekyll::Patreon::Generator::PatreonGenerator.getConfig
             
@@ -37,7 +36,7 @@ module Jekyll
           end
 
           source = "<script>" + File.read(File.join(@inc, "js", "patreon.js")) + "</script>"
-          source += File.read(File.join(@inc, "design_" + @config['design'] + ".html")).interpolate({ json: @json, showgoaltext: @config['showgoaltext'], toptext: @config['toptext'], metercolor: @config['metercolor'], bottomtext: @config['bottomtext'], patreon_button: @config['patreon_button'] })
+          source += File.read(File.join(@inc, "design_" + @config['design'] + ".html")).interpolate({ json: translateJson(context, @json), showgoaltext: @config['showgoaltext'], toptext: @config['toptext'], metercolor: @config['metercolor'], bottomtext: @config['bottomtext'], patreon_button: @config['patreon_button'] })
           
           if @config['showbutton']
             # source += "<script>" + File.read(File.join(@inc, "js", "becomePatronButton.bundle.js")).force_encoding('UTF-8') + "</script>" 
@@ -46,12 +45,51 @@ module Jekyll
             
           source += "<style>" + File.read(File.join(@inc, "css", "design_" + @config['design'] + ".css")) + "</style>"
           source += "<style>" + File.read(File.join(@inc, "css", "common.css")) + "</style>"
+          # source += "<script>" + Jekyll::Patreon.get_language(context) + "</script>"
+            
+          # trJson = translateJson(context, @json)
+          # source += "<script>#{trJson}</script>"
 
           source
         end
 
         def raiseError()
             raise RuntimeError, "An error occurred getting the ID from your Patreon profile"
+        end
+          
+        def translateJson(context, jsonStr)
+          language = Jekyll::Patreon.get_language(context)
+            
+          if language.to_s.empty?
+             return jsonStr.escape_json  
+          end    
+            
+          json = Jekyll::Patreon::Parsers::PatreonParser.parseJson(jsonStr)
+
+          incl = json["included"]
+          startIndex = 0
+            
+          incl.each_with_index do |item, index|
+             startIndex = index
+             break if item["type"] == "goal"
+          end
+            
+          # Jekyll.logger.info "Patreon lang path:", File.join('..', '..', '..', '_data', 'lang', "#{language}.yml")
+          file = File.expand_path(File.join('..', '..', '..', '..', '..', '..', '_data', 'lang', "#{language}.yml"), __FILE__)
+            
+          # Jekyll.logger.info "Patreon lang path:", file
+
+          ymlConf = YAML.load_file(file)
+          # Jekyll.logger.info "Patreon config:", ymlConf.to_s
+          # return ymlConf.to_s.escape_json
+            
+          for index in (startIndex..incl.length - 1)
+             i = index - startIndex
+             # Jekyll.logger.info "Patreon accesor:", "patreon_goal_#{i}"
+             json["included"][index]["attributes"]["description"] = ymlConf["patreon_goal_#{i}"]
+          end
+        
+          return JSON.dump(json).escape_json
         end
       end
   end
