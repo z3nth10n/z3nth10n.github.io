b2ce044060a9370232509750c0bc49301fd00a6b
z3nth10n <z3nth10n@gmail.com>
2019-03-31 13:46:35 +0000

    Updated circle.yml

--- script/get_scheduled_posts.sh
+++ script/get_scheduled_posts.sh
@@ -5,19 +5,21 @@ IFS="="
 schedule_date=""
 schedule_post=""
 
-while read -r name value
-do
-    if [[ $name == "schedule_date" ]]; then
-        schedule_date="$value"
-    elif [[ $name == "schedule_post" ]]; then
-        schedule_post="$value"
+if [[ -d ~/repo/scheduled-posts ]]; then
+    while read -r name value
+    do
+        if [[ $name == "schedule_date" ]]; then
+            schedule_date="$value"
+        elif [[ $name == "schedule_post" ]]; then
+            schedule_post="$value"
 
-        # Then do the magic
-        if [[ $schedule_date == "$(date +%Y-%m-%d)" && -d ~/repo/ ]]; then
-            echo "Publishing post: $schedule_post"
-            mv ~/repo/scheduled-posts/$schedule_post ~/repo/_posts/$schedule_post
-        elif [[ ! -d ~/repo/ ]]; then
-            echo "You have bad configuration on circle.yml ($schedule_post)"
+            # Then do the magic
+            if [[ $schedule_date == "$(date +%Y-%m-%d)" && -d ~/repo/ ]]; then
+                echo "Publishing post: $schedule_post"
+                mv ~/repo/scheduled-posts/$schedule_post ~/repo/_posts/$schedule_post
+            elif [[ ! -d ~/repo/ ]]; then
+                echo "You have bad configuration on circle.yml ($schedule_post)"
+            fi
         fi
-    fi
-done < "script/sch.ini"
+    done < "script/sch.ini"
+fi
