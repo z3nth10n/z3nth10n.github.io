bd2000fe921c815ee1a450da8390c57c5ffd5e74
Ikillnukes <ikillnukes1@gmail.com>
2019-03-31 15:42:10 +0200

    Trying without exiting

--- script/get_scheduled_posts.sh
+++ script/get_scheduled_posts.sh
@@ -5,23 +5,21 @@ IFS="="
 schedule_date=""
 schedule_post=""
 
-if [[ ! -d ~/repo/scheduled-posts ]]; then
-    exit 1
-fi
+if [[ -d ~/repo/scheduled-posts ]]; then
+    while read -r name value
+    do
+        if [[ $name == "schedule_date" ]]; then
+            schedule_date="$value"
+        elif [[ $name == "schedule_post" ]]; then
+            schedule_post="$value"
 
-while read -r name value
-do
-    if [[ $name == "schedule_date" ]]; then
-        schedule_date="$value"
-    elif [[ $name == "schedule_post" ]]; then
-        schedule_post="$value"
-
-        # Then do the magic
-        if [[ $schedule_date == "$(date +%Y-%m-%d)" && -d ~/repo/ ]]; then
-            echo "Publishing post: $schedule_post"
-            mv ~/repo/scheduled-posts/$schedule_post ~/repo/_posts/$schedule_post
-        elif [[ ! -d ~/repo/ ]]; then
-            echo "You have bad configuration on circle.yml ($schedule_post)"
+            # Then do the magic
+            if [[ $schedule_date == "$(date +%Y-%m-%d)" && -d ~/repo/ ]]; then
+                echo "Publishing post: $schedule_post"
+                mv ~/repo/scheduled-posts/$schedule_post ~/repo/_posts/$schedule_post
+            elif [[ ! -d ~/repo/ ]]; then
+                echo "You have bad configuration on circle.yml ($schedule_post)"
+            fi
         fi
-    fi
-done < "script/sch.ini"
+    done < "script/sch.ini"
+fi
